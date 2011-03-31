a7270c20a96ed8e25c8aee9cb053da4bfbe8845a
rich <atkinsonr@gmail.com>
2011-03-31 15:40:00 +1100

    oauth to goplan working, need to know the username.

---
+++ extendedauth/views.py
@@ -0,0 +1,97 @@
+# Python
+import oauth2 as oauth
+import cgi
+
+# Django
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+# Project
+from extendedauth.models import Profile
+
+ 
+consumer = oauth.Consumer(settings.OAUTH_KEY, settings.OAUTH_SECRET)
+client = oauth.Client(consumer)
+
+request_token_url = settings.OAUTH_URL_REQUEST_TOKEN
+access_token_url = settings.OAUTH_URL_ACCESS_TOKEN
+authenticate_url = settings.OAUTH_URL_AUTHORIZE
+
+def oauth_login(request):
+    # Step 1. Get a request token from Twitter.
+    resp, content = client.request(request_token_url, "GET")
+    if resp['status'] != '200':
+        raise Exception("Invalid response from oauth provider.")
+
+    # Step 2. Store the request token in a session for later use.
+    request.session['request_token'] = dict(cgi.parse_qsl(content))
+
+    # Step 3. Redirect the user to the authentication URL.
+    url = "%s?oauth_token=%s" % (authenticate_url,
+        request.session['request_token']['oauth_token'])
+
+    return HttpResponseRedirect(url)
+
+@login_required
+def oauth_logout(request):
+    # Log a user out using Django's logout function and redirect them
+    # back to the homepage.
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def oauth_authenticated(request):
+    # Step 1. Use the request token in the session to build a new client.
+    token = oauth.Token(request.session['request_token']['oauth_token'],
+        request.session['request_token']['oauth_token_secret'])
+    client = oauth.Client(consumer, token)
+
+    # Step 2. Request the authorized access token from Twitter.
+    resp, content = client.request(access_token_url, "GET")
+    if resp['status'] != '200':
+        print content
+        raise Exception("Invalid response from oauth provider.")
+
+    """
+    This is what you'll get back from Twitter. Note that it includes the
+    user's user_id and screen_name.
+    {
+        'oauth_token_secret': 'IcJXPiJh8be3BjDWW50uCY31chyhsMHEhqJVsphC3M',
+        'user_id': '120889797', 
+        'oauth_token': '120889797-H5zNnM3qE0iFoTTpNEHIz3noL9FKzXiOxwtnyVOD',
+        'screen_name': 'heyismysiteup'
+    }
+    """
+    access_token = dict(cgi.parse_qsl(content))
+    print access_token
+
+    # Step 3. Lookup the user or create them if they don't exist.
+    try:
+        user = User.objects.get(username=access_token['screen_name'])
+    except User.DoesNotExist:
+        # When creating the user I just use their screen_name@twitter.com
+        # for their email and the oauth_token_secret for their password.
+        # These two things will likely never be used. Alternatively, you 
+        # can prompt them for their email here. Either way, the password 
+        # should never be used.
+        user = User.objects.create_user(access_token['screen_name'],
+            '%s@twitter.com' % access_token['screen_name'],
+            access_token['oauth_token_secret'])
+
+        # Save our permanent token and secret for later.
+        profile = Profile()
+        profile.user = user
+        profile.oauth_token = access_token['oauth_token']
+        profile.oauth_secret = access_token['oauth_token_secret']
+        profile.save()
+
+    # Authenticate the user and log them in using Django's pre-built 
+    # functions for these things.
+    user = authenticate(username=access_token['screen_name'],
+        password=access_token['oauth_token_secret'])
+    login(request, user)
+
+    return HttpResponseRedirect('/')
---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from extendedauth.views import oauth_login, oauth_logout, \
+     oauth_authenticated
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/?$', oauth_login),
+    url(r'^logout/?$', oauth_logout),
+    url(r'^login/authenticated/?$', oauth_authenticated),
+)
