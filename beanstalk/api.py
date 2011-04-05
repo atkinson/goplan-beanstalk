83d162fd55affe0963d0c2d6ef6fae021aa580b0
rich <atkinsonr@gmail.com>
2011-04-05 16:03:14 +1000

    parsing xml form enumerate repos

--- beanstalk/api.py
+++ beanstalk/api.py
@@ -21,14 +21,23 @@ class Beanstalk(object):
     def _open_url(self, url):
         request = urllib2.Request(url) 
         request.add_header("Authorization", self.basic_auth)
-        return urllib2.urlopen(request)
+        return urllib2.urlopen(request).read()
         
     def enumerate_repos(self):
         """ List all repositories"""
         url = urllib.basejoin(self.api_base, 'repositories.xml')
         req = self._open_url(url)
-        print req.read()
-        dom = minidom.parseString(req.read())
+        dom = minidom.parseString(req)
+        repos = dom.getElementsByTagName('repository')
+        results = []
+        for repo in repos:
+            results.append({
+                  'id': repo.getElementsByTagName('id')[0].firstChild.data,
+                'name': repo.getElementsByTagName('name')[0].firstChild.data,
+               'title': repo.getElementsByTagName('title')[0].firstChild.data,
+                'type': repo.getElementsByTagName('type')[0].firstChild.data
+            })
+        return results
 
     def get_repo(self, repo):
         url = urllib.basejoin(self.api_base, 'repositories/%s.xml' % repo)
--- goplan/models.py
+++ goplan/models.py
@@ -4,6 +4,7 @@ from django.db import models
     
 class Repo(models.Model):
     repo_id = models.CharField(max_length = 32, unique=True)
+    repo_name = models.CharField(max_length = 64)
     repo_type = models.CharField(max_length = 32)
     repo_title = models.TextField(max_length = 128)
 
